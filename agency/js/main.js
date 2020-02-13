67a801fd49db3f0a285d1e1ade85a506a9d4563b
freebrax1111 <53756833+freebrax1111@users.noreply.github.com>
2020-02-13 19:51:17 +0300

    optimazed

--- agency/js/main.js
+++ agency/js/main.js
@@ -15,22 +15,17 @@ $(document).ready(function(){
 	$('.menu__burger-bg').click(function(){
 		$('.menu').removeClass('menu__active');
 		$('.menu__burger-bg').removeClass('menu__burger-bg_active');
-	})
+	});
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+	var newsTop = $('.news').offset().top;
+	$(window).bind('scroll',function(){
+		var windowTop = $(this).scrollTop();
+		if (windowTop>newsTop) {
+			$('.footer__map').html('<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2540.124208598269!2d30.364922015155003!3d50.45741169474517!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40d4cc919501b4ef%3A0x71a476f68f4c8246!2z0YPQuy4g0KTQtdC-0LTQvtGA0Ysg0J_Rg9GI0LjQvdC-0LksIDEzLCDQmtC40LXQsiwg0KPQutGA0LDQuNC90LAsIDAyMDAw!5e0!3m2!1sru!2sru!4v1563800683514!5m2!1sru!2sru" frameborder="0" style="border:0" allowfullscreen></iframe>')
+			$(this).unbind('scroll');
+		}
+	});
 
 
 });
